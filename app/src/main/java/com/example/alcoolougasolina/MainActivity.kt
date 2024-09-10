15263514a57ff2313f27bc2bdd1c61b009ba7a01
BrunoEskinaziUFC <brunoeskinazi@alu.ufc.br>
2024-09-09 23:31:41 -0300

    Adicionando ao repositório

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -5,20 +5,58 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
+    lateinit var tvVale: TextView
+    lateinit var etAlcool: EditText
+    lateinit var etGasolina: EditText
+    lateinit var swPorcentagem: Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvVale = findViewById(R.id.tvVale)
+        etAlcool = findViewById(R.id.edAlcool)
+        etGasolina = findViewById(R.id.edGasolina)
+        swPorcentagem = findViewById(R.id.swPercentual)
+
+        percentual = 0.7
+
         Log.d("PDM23","No onCreate, $percentual")
 
         val btCalc: Button = findViewById(R.id.btCalcular)
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
-            percentual=0.75
             Log.d("PDM23","No btCalcular, $percentual")
+
+            if (etAlcool.text.isNotBlank() and etGasolina.text.isNotBlank()){
+                val valorAlcool = etAlcool.text.toString().toDouble()
+                val valorGasolina = etGasolina.text.toString().toDouble()
+                if (valorGasolina*percentual >= valorAlcool){
+                    tvVale.text = "O Álcool tá valendo a pena"
+                }else{
+                    tvVale.text = "A Gasolina tá valendo a pena"
+                }
+            }
+
         })
+
+        swPorcentagem.setOnClickListener{
+            if (swPorcentagem.isChecked){
+                percentual = 0.75
+            }else{
+                percentual = 0.7
+            }
+            swPorcentagem.text = percentual.toString()
+        }
+
+
     }
 override fun onResume(){
     super.onResume()
